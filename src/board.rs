b565c1d80d2f11d353ae02f6a198b29bec4c1f28
Adao <anthonydaoy@gmail.com>
2025-06-29 15:55:32 -0700

    Refactored and separated board fill logic into separate system. Implemented asset load for square images into separated struct and impl

--- src/board.rs
+++ src/board.rs
@@ -1,49 +1,58 @@
 use bevy::prelude::*; 
 use bevy_ecs_tilemap::prelude::*;
 
+const MAP_LENGTH: u32 = 8;
+
 pub struct BoardPlugin;
 impl Plugin for BoardPlugin {
     fn build(&self, app: &mut App) {
         app
+            .init_resource::<TileHandleSquare>()
             .add_systems(Startup, startup);
+    }
+}
+
+// Loads images for the squares on the board
+#[derive(Deref, Resource)]
+pub struct TileHandleSquare(Handle<Image>);
+
+impl FromWorld for TileHandleSquare {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.resource::<AssetServer>();
+        Self(asset_server.load("images.png"))
     }
 }
 
 fn startup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
+    tile_handle_square: Res<TileHandleSquare>
 ) {
     // Spawn Camera
     commands.spawn(Camera2d);
 
-    let texture_handle: Handle<Image> = asset_server.load("images.png");
-
-    let board_size = TilemapSize { x: 8, y: 8 };
+    let board_size = TilemapSize { 
+        x: MAP_LENGTH,
+        y: MAP_LENGTH,
+    };
 
     // Create a tilemap entity a little early, this entity will tell each tile which tilemap entity
     // it is associated with. Using TilemapId component on each tile, then we will insert the TilemapBundle 
     // bundle on the entity which will have necessary components such as TileStorage... 
     let tilemap_entity = commands.spawn_empty().id(); 
     
+    let tilemap_id = TilemapId(tilemap_entity);
     // To create a map, we will need the 'TileStorage' Component.
     // This component is a grid of tile entities and will track each of the tiles in the world. 
     // If multiple layers of tiles exist you would have 
     let mut tile_storage = TileStorage::empty(board_size);
 
-    // Spawn the elements of the tilemap!
-    for x in 0..board_size.x {
-        for y in 0..board_size.y {
-            let tile_pos = TilePos {x, y};
-            let tile_entity = commands
-                .spawn(TileBundle {
-                    position: tile_pos,
-                    tilemap_id: TilemapId(tilemap_entity),
-                    ..Default::default()
-                })
-                .id();
-            tile_storage.set(&tile_pos, tile_entity);
-        }
-    }
+    fill_board(
+        TileTextureIndex(0),
+        board_size,
+        tilemap_id,
+        &mut commands,
+        &mut tile_storage,
+    );
 
     let tile_size = TilemapTileSize {x: 64.0, y: 64.0};
     let grid_size = tile_size.into();
@@ -54,9 +63,36 @@ fn startup(
         map_type,
         size: board_size,
         storage: tile_storage,
-        texture: TilemapTexture::Single(texture_handle),
+        texture: TilemapTexture::Single(tile_handle_square.clone()),
         tile_size,
         anchor: TilemapAnchor::Center,
         ..Default::default()
     });
 }
+
+fn fill_board(
+    texture_index: TileTextureIndex,
+    size: TilemapSize,
+    tilemap_id: TilemapId,
+    commands: &mut Commands,
+    tile_storage: &mut TileStorage,
+) {
+    commands.entity(tilemap_id.0).with_children(|parent| {
+        for x in 0..size.x {
+            for y in 0..size.y {
+                let tile_pos = TilePos{ x, y};
+                let tile_entity = parent
+                    .spawn(TileBundle {
+                        position: tile_pos,
+                        tilemap_id,
+                        texture_index,
+                        ..Default::default()
+                    })    
+                    .id();
+                tile_storage.set(&tile_pos, tile_entity);
+            }
+        }
+    });
+}
+// END OF BOARD SETUP
+
