78dd790c3f67c1a411751d57bc316ce07598b332
Adao <anthonydaoy@gmail.com>
2025-08-02 16:31:08 -0700

    finished setting up the white pieces

--- src/pieces.rs
+++ src/pieces.rs
@@ -44,13 +44,126 @@ fn spawn_pieces(
                     piece_type: PieceType::Rook,
                     // TODO: position variable later
                 }); 
-            }
+    }
+
+    if let Some(&(x, y)) = board.positions.get(&"A2".to_string()) {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("white_knight.png")),
+            Transform::from_xyz(x, y, 1.0),
+        ))
+            .insert(
+                Piece{
+                    color: PieceColor::White,
+                    piece_type: PieceType::Knight,
+                    // TODO: position variable later
+                }); 
+    }
+
+    if let Some(&(x, y)) = board.positions.get(&"A3".to_string()) {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("white_bishop.png")),
+            Transform::from_xyz(x, y, 1.0),
+        ))
+            .insert(
+                Piece{
+                    color: PieceColor::White,
+                    piece_type: PieceType::Bishop,
+                    // TODO: position variable later
+                }); 
+    }
+
+    if let Some(&(x, y)) = board.positions.get(&"A4".to_string()) {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("white_queen.png")),
+            Transform::from_xyz(x, y, 1.0),
+        ))
+            .insert(
+                Piece{
+                    color: PieceColor::White,
+                    piece_type: PieceType::Queen,
+                    // TODO: position variable later
+                }); 
+    }
+
+    if let Some(&(x, y)) = board.positions.get(&"A5".to_string()) {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("white_king.png")),
+            Transform::from_xyz(x, y, 1.0),
+        ))
+            .insert(
+                Piece{
+                    color: PieceColor::White,
+                    piece_type: PieceType::King,
+                    // TODO: position variable later
+                }); 
+    }
+
+    if let Some(&(x, y)) = board.positions.get(&"A6".to_string()) {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("white_bishop.png")),
+            Transform::from_xyz(x, y, 1.0),
+        ))
+            .insert(
+                Piece{
+                    color: PieceColor::White,
+                    piece_type: PieceType::Bishop,
+                    // TODO: position variable later
+                }); 
+    }
+
+    if let Some(&(x, y)) = board.positions.get(&"A7".to_string()) {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("white_knight.png")),
+            Transform::from_xyz(x, y, 1.0),
+        ))
+            .insert(
+                Piece{
+                    color: PieceColor::White,
+                    piece_type: PieceType::Knight,
+                    // TODO: position variable later
+                }); 
+    }
+
+    if let Some(&(x, y)) = board.positions.get(&"A8".to_string()) {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("white_rook.png")),
+            Transform::from_xyz(x, y, 1.0),
+        ))
+            .insert(
+                Piece{
+                    color: PieceColor::White,
+                    piece_type: PieceType::Rook,
+                    // TODO: position variable later
+                }); 
+    }
+}
+
+fn spawn_pawns(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    board: Res<Placement>,
+) {
+    for i in 1..=8 {
+        if let Some(&(x, y)) = board.positions.get(&format!("B{}", i)) {
+            commands.spawn((
+                Sprite::from_image(asset_server.load("white_pawn.png")),
+                Transform::from_xyz(x, y, 1.0),
+            ))
+                .insert(
+                    Piece{
+                        color:PieceColor::White,
+                        piece_type: PieceType::Pawn,
+                        // TODO: position variable later
+                });
+        } 
+    }
 }
 
 pub struct PiecesPlugin;
 impl Plugin for PiecesPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(Startup, spawn_pieces.after(setup_placement));
+            .add_systems(Startup, spawn_pieces.after(setup_placement))
+            .add_systems(Startup, spawn_pawns.after(setup_placement));
     }
 }
--- src/position.rs
+++ src/position.rs
@@ -12,6 +12,22 @@ pub struct Placement {
 pub fn setup_placement(mut commands: Commands) {
     let mut map: HashMap<String, (f32, f32)> = HashMap::new(); 
     map.insert("A1".to_string(), (-350.0, -350.0));
+    map.insert("A2".to_string(), (-250.0, -350.0));
+    map.insert("A3".to_string(), (-150.0, -350.0));
+    map.insert("A4".to_string(), (-50.0, -350.0));
+    map.insert("A5".to_string(), (50.0, -350.0));
+    map.insert("A6".to_string(), (150.0, -350.0));
+    map.insert("A7".to_string(), (250.0, -350.0));
+    map.insert("A8".to_string(), (350.0, -350.0));
+
+    let mut x_pos: f32 = -350.0;
+    let mut y_pos: f32 = -250.0;
+
+    for i in 1..=8 {
+        map.insert(format!("B{}", i), (x_pos, y_pos));
+        x_pos += 100.0; 
+    }
+
     commands.insert_resource(Placement {positions: map});
 }
 
