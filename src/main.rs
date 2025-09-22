8ece16a667489422aa2dbe18d824bd4b68489c6d
Adao <anthonydaoy@gmail.com>
2025-09-22 09:29:58 -0700

    added text for some of the features including keycodes

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,22 @@ fn main() {
             ResetPlugin, 
         ))
         .add_plugins(TilemapPlugin)
+        .add_systems(Startup, spawn_text)
         .run();
 }
 
+fn spawn_text(mut commands: Commands) {
+    commands
+        .spawn(Node {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(12.0),
+            left: Val::Px(12.0),
+            ..default()
+        })
+        .with_child(Text::new(concat!(
+            "Press 4 to which to windowed, and 5 to switch to fullscreen\n",
+            "Press 1, 2, 3 to change resolution sizes\n",
+            "Press esc to reset the board\n",
+            "Press Q(ueen), R(ook), B(ishop), K(night) to promote pawns."
+        )));
+}
