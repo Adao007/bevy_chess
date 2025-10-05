456a12b439fac8daa0e0065b51a0115cd50e6492
Anthony Dao <tony_bazzite@DESKTOP-89V1U9L-66.home>
2025-10-04 21:39:16 -0700

    added ladybug for white side (bishop), added consts for fast image replacement

--- src/pieces.rs
+++ src/pieces.rs
@@ -1,61 +1,84 @@
-use bevy::prelude::*;
 use super::position::*;
+use bevy::prelude::*;
+
+// Edit piece png(s) here
+
+// Black Pieces PNG
+const BLACK_PAWN_IMAGE: &str = "black_pawn.png";
+const BLACK_ROOK_IMAGE: &str = "black_rook.png";
+const BLACK_KNIGHT_IMAGE: &str = "black_knight.png";
+const BLACK_BISHOP_IMAGE: &str = "black_bishop.png";
+const BLACK_QUEEN_IMAGE: &str = "black_queen.png";
+const BLACK_KING_IMAGE: &str = "black_king.png";
+// White Pieces PNG
+const WHITE_PAWN_IMAGE: &str = "white_pawn.png";
+const WHITE_ROOK_IMAGE: &str = "white_rook.png";
+const WHITE_KNIGHT_IMAGE: &str = "white_knight.png";
+const WHITE_BISHOP_IMAGE: &str = "ladybug.png";
+const WHITE_QUEEN_IMAGE: &str = "white_queen.png";
+const WHITE_KING_IMAGE: &str = "white_king.png";
 
 pub struct PiecesPlugin;
 impl Plugin for PiecesPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_systems
-                (Startup, spawn_pieces.after(setup_placement))
+        app.add_systems(Startup, spawn_pieces.after(setup_placement))
             .add_systems(Update, promote_black)
             .add_systems(Update, promote_white);
     }
 }
 
-
 #[derive(Clone, Copy, PartialEq)]
 pub enum PieceType {
-    King, 
+    King,
     Queen,
-    Bishop, 
+    Bishop,
     Knight,
     Rook,
 }
 
 #[derive(Component)]
-pub struct BlackPiece; 
+pub struct BlackPiece;
 
 #[derive(Component)]
-pub struct WhitePiece; 
+pub struct WhitePiece;
 
 #[derive(Component)]
-pub struct Pawn; 
+pub struct Pawn;
 
 #[derive(Component)]
-pub struct Movable; 
+pub struct Movable;
 
-fn spawn_pieces(
-    mut commands: Commands, 
-    asset_server: Res<AssetServer>,
-    board: Res<Placement>,
-) {
+fn spawn_pieces(mut commands: Commands, asset_server: Res<AssetServer>, board: Res<Placement>) {
     spawn_white_pieces(&mut commands, &asset_server, &board);
     spawn_pawns(&mut commands, &asset_server, &board);
     spawn_black_pieces(&mut commands, &asset_server, &board);
 }
-
 
 pub fn spawn_black_pieces(
     commands: &mut Commands,
     asset_server: &Res<AssetServer>,
     board: &Res<Placement>,
 ) {
-    let names = vec!["black_rook.png", "black_knight.png", "black_bishop.png",
-                                        "black_queen.png", "black_king.png", "black_bishop.png",
-                                        "black_knight.png", "black_rook.png"];
-    let types = vec![PieceType::Rook, PieceType::Knight, PieceType::Bishop,
-                                    PieceType::King, PieceType::Queen, PieceType::Bishop,
-                                    PieceType::Knight, PieceType::Rook];
+    let names = vec![
+        BLACK_ROOK_IMAGE,
+        BLACK_KNIGHT_IMAGE,
+        BLACK_BISHOP_IMAGE,
+        BLACK_QUEEN_IMAGE,
+        BLACK_KING_IMAGE,
+        BLACK_BISHOP_IMAGE,
+        BLACK_KNIGHT_IMAGE,
+        BLACK_ROOK_IMAGE,
+    ];
+    let types = vec![
+        PieceType::Rook,
+        PieceType::Knight,
+        PieceType::Bishop,
+        PieceType::King,
+        PieceType::Queen,
+        PieceType::Bishop,
+        PieceType::Knight,
+        PieceType::Rook,
+    ];
 
     for i in 1..=8 {
         if let Some(&(x, y)) = board.positions.get(&format!("H{}", i)) {
@@ -67,30 +90,44 @@ pub fn spawn_black_pieces(
                 Movable,
             ));
         }
-    }                             
+    }
 }
 
 pub fn spawn_white_pieces(
-    commands: &mut Commands, 
+    commands: &mut Commands,
     asset_server: &Res<AssetServer>,
     board: &Res<Placement>,
 ) {
-    let names = vec!["white_rook.png", "white_knight.png", "white_bishop.png", 
-                                        "white_queen.png", "white_king.png", "white_bishop.png",
-                                        "white_knight.png", "white_rook.png"];
-    let types = vec![PieceType::Rook, PieceType::Knight, PieceType::Bishop, 
-                                        PieceType::Queen, PieceType::King, PieceType::Bishop, 
-                                        PieceType::Knight, PieceType::Rook];
+    let names = vec![
+        WHITE_ROOK_IMAGE,
+        WHITE_KNIGHT_IMAGE,
+        WHITE_BISHOP_IMAGE,
+        WHITE_QUEEN_IMAGE,
+        WHITE_KING_IMAGE,
+        WHITE_BISHOP_IMAGE,
+        WHITE_KNIGHT_IMAGE,
+        WHITE_ROOK_IMAGE,
+    ];
+    let types = vec![
+        PieceType::Rook,
+        PieceType::Knight,
+        PieceType::Bishop,
+        PieceType::Queen,
+        PieceType::King,
+        PieceType::Bishop,
+        PieceType::Knight,
+        PieceType::Rook,
+    ];
 
     for i in 1..=8 {
         if let Some(&(x, y)) = board.positions.get(&format!("A{}", i)) {
-        commands.spawn((
-            Sprite::from_image(asset_server.load(names[i - 1])),
-            Transform::from_xyz(x, y, 1.0),
-            WhitePiece,
-            Pickable::default(),
-            Movable,
-        ));
+            commands.spawn((
+                Sprite::from_image(asset_server.load(names[i - 1])),
+                Transform::from_xyz(x, y, 1.0),
+                WhitePiece,
+                Pickable::default(),
+                Movable,
+            ));
         }
     }
 }
@@ -104,21 +141,21 @@ pub fn spawn_pawns(
     for i in 1..=8 {
         if let Some(&(x, y)) = board.positions.get(&format!("B{}", i)) {
             commands.spawn((
-                Sprite::from_image(asset_server.load("white_pawn.png")),
+                Sprite::from_image(asset_server.load(WHITE_PAWN_IMAGE)),
                 Transform::from_xyz(x, y, 1.0),
                 WhitePiece,
                 Pawn,
                 Pickable::default(),
                 Movable,
             ));
-        } 
+        }
     }
-    
-    // Spawning the Black Pawns 
+
+    // Spawning the Black Pawns
     for i in 1..=8 {
         if let Some(&(x, y)) = board.positions.get(&format!("G{}", i)) {
             commands.spawn((
-                Sprite::from_image(asset_server.load("black_pawn.png")),
+                Sprite::from_image(asset_server.load(BLACK_PAWN_IMAGE)),
                 Transform::from_xyz(x, y, 1.0),
                 BlackPiece,
                 Pawn,
@@ -131,61 +168,61 @@ pub fn spawn_pawns(
 
 fn promote_black(
     mut commands: Commands,
-    mut pawn_query: Query<(Entity, &mut Sprite, &Transform), (With<Pawn>, With<BlackPiece>)>, 
+    mut pawn_query: Query<(Entity, &mut Sprite, &Transform), (With<Pawn>, With<BlackPiece>)>,
     asset_server: Res<AssetServer>,
-    keys: Res<ButtonInput<KeyCode>>, 
+    keys: Res<ButtonInput<KeyCode>>,
 ) {
     for (entity, mut sprite, piece) in pawn_query.iter_mut() {
-        if (piece.translation.y <= -300.0 && piece.translation.y >= -400.0) &&
-           (piece.translation.x <= 400.0 && piece.translation.x >= -400.0) {
-            //*sprite = Sprite::from_image(asset_server.load("black_queen.png")); 
+        if (piece.translation.y <= -300.0 && piece.translation.y >= -400.0)
+            && (piece.translation.x <= 400.0 && piece.translation.x >= -400.0)
+        {
+            //*sprite = Sprite::from_image(asset_server.load("black_queen.png"));
             if keys.just_released(KeyCode::KeyQ) {
-                *sprite = Sprite::from_image(asset_server.load("black_queen.png")); 
+                *sprite = Sprite::from_image(asset_server.load("black_queen.png"));
                 commands.entity(entity).remove::<Pawn>();
             }
             if keys.just_released(KeyCode::KeyR) {
-                *sprite = Sprite::from_image(asset_server.load("black_rook.png")); 
+                *sprite = Sprite::from_image(asset_server.load("black_rook.png"));
                 commands.entity(entity).remove::<Pawn>();
             }
             if keys.just_released(KeyCode::KeyK) {
-                *sprite = Sprite::from_image(asset_server.load("black_knight.png")); 
+                *sprite = Sprite::from_image(asset_server.load("black_knight.png"));
                 commands.entity(entity).remove::<Pawn>();
             }
             if keys.just_released(KeyCode::KeyB) {
                 *sprite = Sprite::from_image(asset_server.load("black_bishop.png"));
                 commands.entity(entity).remove::<Pawn>();
             }
-            
-        }
-    }
-} 
-
-fn promote_white(
-    mut commands: Commands,
-    mut pawn_query: Query<(Entity, &mut Sprite, &Transform), (With<Pawn>, With<WhitePiece>)>, 
-    asset_server: Res<AssetServer>, 
-    keys: Res<ButtonInput<KeyCode>>, 
-) {
-    for (entity, mut sprite, piece) in pawn_query.iter_mut() {
-        if (piece.translation.y >= 300.0 && piece.translation.y <= 400.0) &&
-           (piece.translation.x <= 400.0 && piece.translation.x >= -400.0){
-            if keys.just_released(KeyCode::KeyQ) {
-                *sprite = Sprite::from_image(asset_server.load("white_queen.png")); 
-                commands.entity(entity).remove::<Pawn>(); 
-            }
-            if keys.just_released(KeyCode::KeyB) {
-                *sprite = Sprite::from_image(asset_server.load("white_bishop.png")); 
-                commands.entity(entity).remove::<Pawn>(); 
-            }
-            if keys.just_released(KeyCode::KeyK) {
-                *sprite = Sprite::from_image(asset_server.load("white_knight.png")); 
-                commands.entity(entity).remove::<Pawn>(); 
-            }
-            if keys.just_released(KeyCode::KeyR) {
-                *sprite = Sprite::from_image(asset_server.load("white_rook.png")); 
-                commands.entity(entity).remove::<Pawn>(); 
-            }
         }
     }
 }
 
+fn promote_white(
+    mut commands: Commands,
+    mut pawn_query: Query<(Entity, &mut Sprite, &Transform), (With<Pawn>, With<WhitePiece>)>,
+    asset_server: Res<AssetServer>,
+    keys: Res<ButtonInput<KeyCode>>,
+) {
+    for (entity, mut sprite, piece) in pawn_query.iter_mut() {
+        if (piece.translation.y >= 300.0 && piece.translation.y <= 400.0)
+            && (piece.translation.x <= 400.0 && piece.translation.x >= -400.0)
+        {
+            if keys.just_released(KeyCode::KeyQ) {
+                *sprite = Sprite::from_image(asset_server.load("white_queen.png"));
+                commands.entity(entity).remove::<Pawn>();
+            }
+            if keys.just_released(KeyCode::KeyB) {
+                *sprite = Sprite::from_image(asset_server.load("white_bishop.png"));
+                commands.entity(entity).remove::<Pawn>();
+            }
+            if keys.just_released(KeyCode::KeyK) {
+                *sprite = Sprite::from_image(asset_server.load("white_knight.png"));
+                commands.entity(entity).remove::<Pawn>();
+            }
+            if keys.just_released(KeyCode::KeyR) {
+                *sprite = Sprite::from_image(asset_server.load("white_rook.png"));
+                commands.entity(entity).remove::<Pawn>();
+            }
+        }
+    }
+}
